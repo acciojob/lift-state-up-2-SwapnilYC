d14a99a1316e13ba5e17c549e1bcfa096f83bff9
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-15 04:29:40 +0000

    class name child given to input element

--- src/components/Child.js
+++ src/components/Child.js
@@ -5,7 +5,7 @@ export default function Child({updateHandler}){
     return (
         <div className="child">
             <h1>Child Component</h1>
-            <input type='text' onChange={(e)=>updateHandler(e.target.value)}></input>
+            <input className="child" type='text' onChange={(e)=>updateHandler(e.target.value)}></input>
         </div>
     );
 }
