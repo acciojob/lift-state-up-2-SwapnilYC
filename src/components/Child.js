dfb4c247b044f340ab95ef62b9842a846467c29c
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-06-15 04:27:09 +0000

    name changed of child class

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 export default function Child({updateHandler}){
     return (
-        <div className="chold">
+        <div className="child">
             <h1>Child Component</h1>
             <input type='text' onChange={(e)=>updateHandler(e.target.value)}></input>
         </div>
